bcc2718f2141ce35109f62a7ce55cf1b2fe68c9e
Alexkcshing <alexkcshing@gmail.com>
2019-04-09 10:48:22 +0100

    updated Department and Project model class

--- src/main/java/com/codeclan/example/employeerelations/models/Department.java
+++ src/main/java/com/codeclan/example/employeerelations/models/Department.java
@@ -1,4 +1,60 @@
 package com.codeclan.example.employeerelations.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="departments")
+
 public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String departmentName;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "department")
+    private ArrayList<Employee> employees;
+
+
+    public Department(){
+    }
+
+    public Department(String department){
+        this.departmentName = department;
+        this.employees = new ArrayList<>();
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void addEmployees(Employee employees) {
+        this.employees.add(employees);
+    }
+    public void setEmployees(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/codeclan/example/employeerelations/models/Project.java
+++ src/main/java/com/codeclan/example/employeerelations/models/Project.java
@@ -1,4 +1,78 @@
 package com.codeclan.example.employeerelations.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "projects")
 public class Project {
+    @Column
+    private String projectName;
+    @Column
+    private int numberOfDays;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "employees_projects",
+            joinColumns = {@JoinColumn(
+                    name = "project_id",
+                    nullable = false,
+                    updatable = false)
+            },
+            inverseJoinColumns = {@JoinColumn(
+                    name = "employee_id",
+                    nullable = false,
+                    updatable =false
+            )}
+    )
+    private ArrayList<Employee> employees;
+
+    public Project(){};
+
+    public Project(String projectName, int numberOfDays){
+        this.projectName = projectName;
+        this.employees = new ArrayList<>();
+        this.numberOfDays = numberOfDays;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public int getNumberOfDays() {
+        return numberOfDays;
+    }
+
+    public void setNumberOfDays(int numberOfDays) {
+        this.numberOfDays = numberOfDays;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public void addEmployee(Employee employee){
+        this.employees.add(employee);
+    }
 }
