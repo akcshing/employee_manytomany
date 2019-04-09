f48adc0090909e9c0000b7a80ccef5f48cceebca
Alexkcshing <alexkcshing@gmail.com>
2019-04-09 09:58:55 +0100

    lab start

---
+++ src/main/java/com/codeclan/example/employeerelations/EmployeerelationsApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employeerelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeerelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeerelationsApplication.class, args);
+	}
+
+}
